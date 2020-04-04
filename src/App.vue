9f77daea3335df50558d1aa41adb333fe85eb949
Brouce Song <songwenwen@outlook.com>
2020-04-04 11:24:05 +0800

    add right

--- src/App.vue
+++ src/App.vue
@@ -34,12 +34,26 @@
         </el-carousel>
       </el-header>
       <el-main>
-        <el-row :gutter="12" v-for="v in lists" :key="v">
-          <el-col span="15" :offset="4">
-            <el-card class="content-list-card">
-                {{v }}
-            </el-card>
-          </el-col>
+        <el-row>
+            <el-col :span="13" :offset="2">
+                <el-row :gutter="12" v-for="v in lists" :key="v">
+                    <el-col>
+                        <el-card class="content-list-card">
+                            {{v }}
+                        </el-card>
+                    </el-col>
+                </el-row>
+            </el-col>
+            <el-col :span="6" :offset="1">
+                <el-card class="right-hot-card">
+                  <div slot="header" >
+                    <span>热门推荐</span>
+                  </div>
+                  <div v-for="v in lists" :key="v" class="hot-card-list">
+                    {{v | ellipsis(v)}}
+                  </div>
+                </el-card>
+            </el-col>
         </el-row>
       </el-main>
       <el-footer>
@@ -50,10 +64,12 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'App',
   data () {
     return {
+      musicDisc: null,
       activeIndex: '1',
       banners: [
         '/static/image/banner_1.jpg',
@@ -75,12 +91,39 @@ export default {
         '人生在世，我们常常产生想解释点什么的想法。然而，一旦解释起来，却发现任何人解释都是那样的苍白无力，甚至还会越抹越黑。因此，做人不需要解释，便成为智者的选择。山不解释自己的高度，并不影响它的耸立云端；海不解释自己的深度，并不影响它容纳百川；地不解释自己的厚度，但没有谁能取代她作为承载万物的地位……',
         '人生在世，我们常常产生想解释点什么的想法。然而，一旦解释起来，却发现任何人解释都是那样的苍白无力，甚至还会越抹越黑。因此，做人不需要解释，便成为智者的选择。山不解释自己的高度，并不影响它的耸立云端；海不解释自己的深度，并不影响它容纳百川；地不解释自己的厚度，但没有谁能取代她作为承载万物的地位……',
         '人生在世，我们常常产生想解释点什么的想法。然而，一旦解释起来，却发现任何人解释都是那样的苍白无力，甚至还会越抹越黑。因此，做人不需要解释，便成为智者的选择。山不解释自己的高度，并不影响它的耸立云端；海不解释自己的深度，并不影响它容纳百川；地不解释自己的厚度，但没有谁能取代她作为承载万物的地位……'
-      ]
+      ],
+      username: 'BrouceSong',
+      gits: []
     }
+  },
+  filters: {
+    ellipsis (value) {
+      if (!value) return ''
+      if (value.length > 20) {
+        return value.slice(0, 20) + '...'
+      }
+      return value
+    }
+  },
+  mounted () {
+    this.loadGithub()
   },
   methods: {
     handleSelect (key, keyPath) {
       console.log(key, keyPath)
+    },
+    loadGithub () {
+      var _This = this
+      axios
+        .get('https://api.github.com/users/' + this.username)
+        .then(function (response) {
+          _This.gits = response.data
+          console.log('succeed', _This.gits)
+        })
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
     }
   }
 }
@@ -110,5 +153,18 @@ export default {
   width:100%;
   height:auto;
   margin: 10px auto;
+}
+.img {
+    background-repeat: no-repeat;
+    background-position:center center;
+    background-size: cover;
+    width:165px;
+    height:105px;
+}
+.right-hot-card {
+  text-align: left;
+}
+.hot-card-list {
+  margin:0px 10px 10px 0
 }
 </style>
